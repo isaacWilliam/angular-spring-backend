505abe3de0cf3bec33c585a169914c76c723a17f
isaac <izakew19@gmail.com>
2024-03-14 12:47:20 -0300

    Primeiro commit

---
+++ src/main/java/com/example/angularspringbackend/AngularSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/angularspringbackend/AngularSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
